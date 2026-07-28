b535d7b5651292a8332d1bf162f4d620e23e5cd8
Sandeep-bachu123 <juug25btech16628@jainuniversity.ac.in>
2026-07-28 20:55:36 +0530

    Add basic calculator operations in cal.py

---
+++ cal.py
@@ -0,0 +1,9 @@
+x=int(input("Enter the 1st number: "))
+y=int(input("Enter the 2nd number: "))
+print("addition", x+y)  
+print("subtraction", x-y)
+print("multiplication", x*y)
+print("division", x/y) # Division
+print("floor division", x//y) # Floor Division
+print("modulus", x%y)
+print("Exponentiation", x**y)
